eb9c205289e44b607120138725035b5c74c1c62f
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2023-11-16 02:29:20 -0300

    define um base path para o front controller da aplicação

--- public/index.php
+++ public/index.php
@@ -9,6 +9,8 @@ use Cascata\Framework\Http\Kernel;
 use Cascata\Framework\Http\Request;
 use Cascata\Framework\Http\Response;
 
+define('BASE_PATH', dirname(__DIR__));
+
 require_once __DIR__ . "/../vendor/autoload.php";
 
 // request received
