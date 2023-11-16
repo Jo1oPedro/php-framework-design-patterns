b12821591d829d332ff2fc3349297bd10baff783
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2023-11-15 21:04:41 -0300

    adiciona response class

--- public/index.php
+++ public/index.php
@@ -5,14 +5,20 @@ global para a nossa aplicação. O próprio .htaccess redireciona as requisiçõ
 */
 declare(strict_types=1);
 
+use Cascata\Framework\Http\Request;
+use Cascata\Framework\Http\Response;
+
 require_once __DIR__ . "/../vendor/autoload.php";
 
 // request received
-$request = \Cascata\Framework\Http\Request::createFromGlobals();
-
-dd($request);
+$request = Request::createFromGlobals();
+//dd($request);
 
 // perform some logic
 
 // send response (string of content)
-echo 'Hello world 2';
+$content = '<h1>Hello world</h1>';
+
+$response = new Response(content: $content, status: 200, headers: []);
+
+$response->send();
