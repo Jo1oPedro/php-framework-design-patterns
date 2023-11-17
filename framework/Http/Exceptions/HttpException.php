6c4894f46a52feaee953eb2b1ac6eb2a689a0514
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2023-11-17 01:23:30 -0300

    adiciona exceptions personalizadas para http

---
+++ framework/Http/Exceptions/HttpException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Cascata\Framework\Http\Exceptions;
+
+class HttpException extends \Exception
+{
+
+}
---
+++ framework/Http/Exceptions/HttpRequestMethodException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Cascata\Framework\Http\Exceptions;
+
+class HttpRequestMethodException extends HttpException
+{
+
+}
