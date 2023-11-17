b2d0c41d838df4063e06ad0c39d18c813bdc83b7
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2023-11-17 00:36:19 -0300

    refatora a forma de chamar uma função de um handler no kernel http

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Controller\HomeController;
+use App\Controller\PostsController;
 
 return [
-    ['GET', 'x', [HomeController::class, 'index']]
+    ['GET', 'x', [HomeController::class, 'index']],
+    ['GET', 'posts/{id:\d+}', [PostsController::class, 'show']]
 ];
