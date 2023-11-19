585f692d628e0b3f7338c1ba7b81520a432d2c8b
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2023-11-19 19:27:09 -0300

    minor changes

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Cascata\Framework\Routing\RouteGrouper;
 
 $routeGrouper = new RouteGrouper();
 
-$routeGrouper->addRoute('GET', 'x', [HomeController::class, 'index']);
+$routeGrouper->addRoute('GET', '/x', [HomeController::class, 'index']);
 $routeGrouper->addRoute('GET', 'posts/{id:\d+}', [PostsController::class, 'show']);
 $routeGrouper->addRoute('POST', 'post/{id:\d+}', [PostsController::class, 'show']);
 $routeGrouper->addRoute('GET', 'post/{id:\d+}', function($id) {
@@ -20,3 +20,6 @@ $routeGrouper->addRoute('POST', 'teste', [PostsController::class, 'show']);
     ['GET', 'posts/{id:\d+}', [PostsController::class, 'show']],
     ['POST', 'post/{id:\d+}', [PostsController::class, 'show']],
 ];*/
+
+
+// para rodar o projeto: php -S localhost:8080 -t public public/index.php
