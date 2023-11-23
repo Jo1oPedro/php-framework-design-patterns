94a9142740326d9c3507334af748e072ce6c506e
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2023-11-23 00:08:35 -0300

    adiciona metodo setRoutes para interface de RouterInterface

--- framework/src/Routing/RouterInterface.php
+++ framework/src/Routing/RouterInterface.php
@@ -7,4 +7,5 @@ use Cascata\Framework\Http\Request;
 interface RouterInterface
 {
     public function dispatch(Request $request): array;
+    public function setRoutes(RouteGrouper $routeGrouper): void;
 }
