b582ad1317a50707782beebd09ad9596e71e29b4
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2023-11-23 00:23:39 -0300

    Adiciona service locator pattern ao kernel para que a instanciação de controllers no Router a partir de uma requisição possa ocorrer de forma automatica e com autowiring através da reflection no container do service.php

--- framework/src/Http/Kernel.php
+++ framework/src/Http/Kernel.php
@@ -4,18 +4,20 @@ namespace Cascata\Framework\Http;
 
 use Cascata\Framework\Http\Exceptions\HttpException;
 use Cascata\Framework\Routing\RouterInterface;
+use Psr\Container\ContainerInterface;
 
 class Kernel
 {
     public function __construct(
-        private readonly RouterInterface $router
+        private readonly RouterInterface $router,
+        private readonly ContainerInterface $container
     ) {
     }
 
     public function handle(Request $request): Response
     {
         try {
-            [$routeHandler, $vars] = $this->router->dispatch($request);
+            [$routeHandler, $vars] = $this->router->dispatch($request, $this->container);
 
             $response = call_user_func_array($routeHandler, $vars);
         } catch (HttpException $exception) {
