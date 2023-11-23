7ca5a3de939129da602df3c2a8e50ef714216688
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2023-11-23 00:37:30 -0300

    Adiciona parametros ao construtor de homecontroller para validar autowiring do container em service.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,14 @@ use Cascata\Framework\Http\Response;
 
 class HomeController
 {
+    public function __construct(
+        private readonly PostsController $postsController,
+        private string $dale = "1"
+    ) {
+        //dd($dale);
+        //dd($postsController);
+    }
+
     public function index(): Response
     {
         $content = '<h1>Hello world</h1>';
