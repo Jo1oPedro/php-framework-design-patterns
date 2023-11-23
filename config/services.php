6c2e0d24e4ef5db6419038c1e5954bac81672c0e
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2023-11-23 00:09:27 -0300

    adiciona container como um arquivo de serviço, afim de configurar a instanciação de objetos e chamada de métodos para tais instanciações em classes especificas

--- config/services.php
+++ config/services.php
@@ -2,4 +2,25 @@
 
 $container = new \League\Container\Container();
 
+## PARAMETERS
+include BASE_PATH . "/routes/web.php";
+
+## SERVICES
+/**
+ * Esse bloco corresponde a autowiring de uma Router responsável pelo processamento das rotas
+ * e equivalencia da request atual com alguma rota sendo inserida como argumento automaticamente na criação
+ * de um kernel responsável pelo processamento da request.
+ */
+$container->add(
+    \Cascata\Framework\Routing\RouterInterface::class,
+    \Cascata\Framework\Routing\Router::class);
+
+/** @var \Cascata\Framework\Routing\RouteGrouper $routeGrouper */
+$container->extend(\Cascata\Framework\Routing\RouterInterface::class)
+    ->addMethodCall('setRoutes', [$routeGrouper]);
+
+$container->add(\Cascata\Framework\Http\Kernel::class)
+    ->addArgument(\Cascata\Framework\Routing\RouterInterface::class);
+/** */
+
 return $container;
