fa44435d3903a30533df38eaa32595c584b94c8d
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2023-12-13 17:54:58 -0300

    adiciona rota padrao para o favicon

--- framework/src/Routing/RouteGrouper.php
+++ framework/src/Routing/RouteGrouper.php
@@ -2,8 +2,30 @@
 
 namespace Cascata\Framework\Routing;
 
+use Cascata\Framework\GlobalContainer\Container;
+use Cascata\Framework\Http\Response;
+
 class RouteGrouper
 {
+    public function __construct(string $faviconPath = "/public/images/favicon.ico")
+    {
+        $this->addRoute('GET', $faviconPath, function() {
+
+            $basePath = Container::getInstance()->get('BASE_PATH');
+            $imagePath = $basePath . "/public/images/favicon.ico";
+            $contentType = mime_content_type($imagePath);
+
+            $response = new Response(
+                file_get_contents($basePath . "/public/images/favicon.ico"),
+                headers: [
+                    'Content-Type' => $contentType,
+                    'Content-Disposition: inline; filename="' . basename($imagePath) . '"'
+                ]
+            );
+            return $response;
+        });
+    }
+
     private array $routes = [];
 
     public function getRoutes(): array
