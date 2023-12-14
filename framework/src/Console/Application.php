804834114248b38acb6f8aa21a90db9f9e445cf3
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2023-12-14 16:05:44 -0300

    atualiza localização das migrations enviada aos comandos

--- framework/src/Console/Application.php
+++ framework/src/Console/Application.php
@@ -32,7 +32,7 @@ class Application
         $options = $this->parseOptions($arguments);
 
         // Execute the command, return the status code
-        $options = array_merge(['BASE_PATH' => $this->container->get('BASE_PATH')], $options);
+        $options = array_merge(['BASE_PATH' => $this->container->get('BASE_PATH') . "/database"], $options);
         $status = $command->execute($options);
 
         // Return the status code
