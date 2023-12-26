2e4c1d34a54ff3d63527eb8aa547005b5112559d
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2023-12-26 10:45:13 -0300

    adiciona comando para rodar todas as factories

---
+++ framework/src/Console/Command/RunFactories.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Cascata\Framework\Console\Command;
+
+use Cascata\Framework\Database\Faker;
+use Cascata\Framework\GlobalContainer\Container;
+
+class RunFactories implements CommandInterface
+{
+    private string $name = 'run:factories';
+
+    public function execute(array $params = []): int
+    {
+        $basePath = Container::getInstance()->get('BASE_PATH');
+        foreach($this->getFactories($basePath) as $factory) {
+            $factoryName = str_replace(".php", "", $factory);
+            $factoryClass = new \ReflectionClass("Database\Factories\\{$factoryName}");
+            if($factoryClass->isSubclassOf(Faker::class)) {
+                $reflectionMethod = $factoryClass->getMethod('create');
+                $factoryInstance = $factoryClass->newInstance();
+                $reflectionMethod->invoke($factoryInstance);
+            }
+        }
+        return 1;
+    }
+
+    private function getFactories(string $basePath)
+    {
+        $onlyFactories = array_slice(scandir($basePath . "/database/factories"), 2);
+        return $onlyFactories;
+    }
+}
