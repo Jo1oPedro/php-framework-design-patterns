80887d1bd1c9c545f9a123fc685284a05a8f9080
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2023-12-26 10:41:28 -0300

    atualiza basePath do comando para criar controllers

--- framework/src/Console/Command/MakeController.php
+++ framework/src/Console/Command/MakeController.php
@@ -2,6 +2,8 @@
 
 namespace Cascata\Framework\Console\Command;
 
+use Cascata\Framework\GlobalContainer\Container;
+
 class MakeController implements CommandInterface
 {
     private string $name = "make:controller";
@@ -13,12 +15,12 @@ class MakeController implements CommandInterface
         if(in_array('resource', $params)) {
             $content = file_get_contents(__DIR__ . "/../../structureTemplates/Controller/ControllerResource.txt");
             $content = str_replace(["{nameHolder}"], [$params["name"]], $content);
-            file_put_contents($params['BASE_PATH'] . "/src/Controller/{$params["name"]}.php", $content);
+            file_put_contents(Container::getInstance()->get('BASE_PATH') . "/src/Controller/{$params["name"]}.php", $content);
             return 0;
         }
 
         $content = str_replace(["{nameHolder}"], [$params["name"]], $content);
-        file_put_contents($params['BASE_PATH'] . "/src/Controller/{$params["name"]}.php", $content);
+        file_put_contents(Container::getInstance()->get('BASE_PATH') . "/src/Controller/{$params["name"]}.php", $content);
         return 0;
     }
 }
